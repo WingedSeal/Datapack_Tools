f684f64c19a83c95723237cecfe3c83b31cceed2
WingedSeal <firm09719@gmail.com>
2021-07-04 11:00:27 +0700

    Add Melee look_at feature

--- datapacktools/tools/melee_look_at.py
+++ datapacktools/tools/melee_look_at.py
@@ -6,9 +6,104 @@ from . import path as _path
 
 
 def generate(subdatapack: SubDatapack, entries: dict):
-    pass
+    namespace = subdatapack.namespace
+    namespace_path = subdatapack.namespace_path
+    subfolder = subdatapack.subfolder_tk.get()
+    item_name = entries['item_name'].get().lower().replace(' ','_')
+    item_nbt = entries['item_nbt'].get().lower().replace(' ','_')
+    if ':' not in item_nbt and item_nbt!="":
+        item_nbt+=':1b'
 
+    #---- MAKE PATHS----
+    if not os.path.exists(os.path.join(namespace_path, 'predicates', subfolder, 'selecteditem')):
+        os.makedirs(os.path.join(namespace_path, 'predicates', subfolder, 'selecteditem'))
+    if not os.path.exists(os.path.join(namespace_path, 'predicates', subfolder, 'utils')):
+        os.makedirs(os.path.join(namespace_path, 'predicates', subfolder, 'utils'))
+    if not os.path.exists(os.path.join(namespace_path, 'advancements', subfolder, 'utils', 'look_at')):
+        os.makedirs(os.path.join(namespace_path, 'advancements', subfolder, 'utils', 'look_at'))
+    item_function_path = os.path.join(namespace_path, 'functions', subfolder, 'look_at', item_name)
+    if not os.path.exists(item_function_path):
+        os.makedirs(item_function_path)
 
+    # Predicate
+    with open(os.path.join(namespace_path, 'predicates', subfolder, 'utils', 'hurt_time.json'), 'w') as hurt_time_json:
+        hurt_time_json.write("""
+{
+    "condition": "minecraft:entity_properties",
+    "entity": "this",
+    "predicate": {
+      "nbt": "{HurtTime:10s}"
+    }
+}
+        """.strip())
+    with open(os.path.join(namespace_path, 'predicates', subfolder, 'utils', 'look_at.json'), 'w') as hurt_time_json:
+        hurt_time_json.write(f"""
+{{
+    "condition": "minecraft:entity_properties",
+    "entity": "this",
+    "predicate": {{
+        "player": {{
+            "looking_at": {{
+                "nbt": "{{Tags:['{namespace}.potential_looked']}}"
+            }}
+        }}
+    }}
+}}
+        """.strip())
+
+    with open(os.path.join(namespace_path, 'predicates', subfolder, 'selecteditem', f'{item_name}.json'), 'w') as selecteditem_file:
+        selecteditem_file.write(f"""
+{{
+    "condition": "minecraft:entity_properties",
+    "entity": "this",
+    "predicate": {{
+      "type": "minecraft:player",
+      "equipment": {{
+        "mainhand": {{
+          "nbt": "{{{item_nbt}}}"
+        }}
+      }}
+    }}
+}}
+        """.strip())
+
+    # Advancements
+    with open(os.path.join(namespace_path, 'advancements', subfolder, 'utils', 'look_at', 'player_hurt_entity.json'), 'w') as player_hurt_entity_json:
+        player_hurt_entity_json.write(f"""
+{{
+    "criteria": {{
+        "trigger": {{
+            "trigger": "minecraft:player_hurt_entity"
+        }}
+    }},
+    "rewards": {{
+        "function": "{namespace}:{subfolder}/melee_look_at/player_hurt_entity"
+    }}
+}}
+        """.strip())
+
+    # Function
+    with open(os.path.join(namespace_path, 'functions', subfolder, 'look_at', 'player_hurt_entity.mcfunction'), 'a') as player_hurt_entity_file:
+        player_hurt_entity_file.write('\n'+f"execute if entity @s[predicate={namespace}:{subfolder}/selecteditem/{item_name}] at @s anchored eyes run function {namespace}:{subfolder}/melee_look_at/{item_name}/init")  
+    
+    # Look At function
+    with open(os.path.join(item_function_path, 'init.mcfunction'), 'w') as init_file:
+        init_file.write(f"""
+tag @s add {namespace}.looker
+execute as @e[distance=..6,tag=!{namespace}.looker] at @s run function {namespace}:{subfolder}/melee_look_at/{item_name}/look
+tag @s remove {namespace}.looker
+execute as @e[distance=..6,tag={namespace}.looked,limit=1,sort=nearest] at @s run function {namespace}:{subfolder}/melee_look_at/{item_name}/exe
+""".strip())
+
+    with open(os.path.join(item_function_path, 'look.mcfunction'), 'w') as look_file:
+        look_file.write(f"""
+tag @s add {namespace}.potential_looked
+execute as @p[tag={namespace}.looker] if predicate {namespace}:{subfolder}/utils/melee_look_at run tag @e[limit=1,sort=nearest] add {namespace}.looked
+tag @s remove {namespace}.potential_looked
+""".strip()) 
+
+    with open(os.path.join(item_function_path, 'exe.mcfunction'), 'w') as exe_file:
+        exe_file.write('say [PLACE HOLDER]')
 
 class MeleeLookAt:
 
@@ -29,8 +124,8 @@ class MeleeLookAt:
 
         label = tk.Label(output_item_frame, text="Item's name: ")
         label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.E)
-        item_id_field = tk.Entry(output_item_frame, width=25)
-        item_id_field.grid(row=0, column=1, columnspan=2, padx=5, pady=5, sticky=tk.W)
+        item_name_field = tk.Entry(output_item_frame, width=25)
+        item_name_field.grid(row=0, column=1, columnspan=2, padx=5, pady=5, sticky=tk.W)
 
         label = tk.Label(output_item_frame, text="NBT: {" )
         label.grid(row=1, column=0, padx=5, pady=5, sticky=tk.E)
@@ -40,7 +135,7 @@ class MeleeLookAt:
         label.grid(row=1, column=2, padx=5, pady=5, sticky=tk.E)
 
         entries = {
-            'item_id': item_id_field,
+            'item_name': item_name_field,
             'item_nbt': item_nbt_field
         }
 
