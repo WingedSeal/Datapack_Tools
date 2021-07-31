5860c3c0fb3cf285db1570d0b603cea4c54bb095
WingedSeal <firm09719@gmail.com>
2021-07-31 13:32:06 +0700

    Clear files

--- datapacktools/__init__.py
+++
@@ -1,4 +0,0 @@
-from .window import Window
-from .datapack import Datapack
-from . import utils
-from .tools import path
--- datapacktools/tools/custom_recipe.py
+++
@@ -1,8 +0,0 @@
-class CustomRecipe:
-
-    def custom_recipe(self):
-        self.window.clear()
-        self.window.label("Custom crafting table recipe", font_size=18, pad=(10,10))
-        self.pack_main_menu()
-
-        self.window.label("Under Construction...", font_size=16, pad=(10,5))
